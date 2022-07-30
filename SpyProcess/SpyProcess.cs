5a413ef5b3829153ca5f755fdb6a622916a7287e
DenisAstashkin <ast.den27@gmail.com>
2022-07-31 02:13:48 +0400

    Add code for SpyProcess

--- SpyProcess/SpyProcess.cs
+++ SpyProcess/SpyProcess.cs
@@ -1,7 +1,26 @@
-﻿namespace SpyProcess
-{
-    public class Class1
-    {
+﻿using System.Diagnostics;
 
+namespace spyprocess
+{
+    public class SpyProcess
+    {
+        public void KillsProcess(string ProcessName)
+        {
+            foreach (var process in Process.GetProcesses())
+            {
+                if(process.ProcessName == ProcessName)
+                {
+                    process.Kill();
+                }
+            }
+        }
+
+        public IEnumerable<(string Name, DateTime StartTime)> GetProcessInfo()
+        {
+            foreach (var process in Process.GetProcesses())
+            {
+                yield return (process.ProcessName, process.StartTime);
+            }
+        }        
     }
 }
