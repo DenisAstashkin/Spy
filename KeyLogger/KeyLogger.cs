6cce495bb82b116ebe67fc60e8dd5e6ba9f077d9
DenisAstashkin <ast.den27@gmail.com>
2022-07-31 01:37:31 +0400

    Init Project

---
+++ KeyLogger/KeyLogger.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace keylogger
+{
+    public class KeyLogger
+    {
+    }
+}
---
+++ UserSpy/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UserSpy.keylogger;
+
+namespace UserSpy
+{    
+    public partial class MainWindow : Window
+    {
+        KeyLogger logger;
+        public MainWindow()
+        {
+            logger = new KeyLogger();
+            InitializeComponent();            
+        }
+    }
+}
