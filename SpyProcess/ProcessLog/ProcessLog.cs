c8ebd35d384658846dd5ed00b1297aafca5df3a3
DenisAstashkin <ast.den27@gmail.com>
2022-07-31 02:31:50 +0400

    Update ProcessLog

--- SpyProcess/ProcessLog/ProcessLog.cs
+++ SpyProcess/ProcessLog/ProcessLog.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using spyprocess.ProcessModel;
 
-namespace SpyProcess.ProcessLog
+namespace spyprocess.processlog
 {
-    internal class PlocessLog
+    public class ProcessLog
     {
+        public ProcessLog() { }
+
+        public bool SaveProcessInfo(string path, ProcessModel)
+        {
+
+        }
     }
 }
