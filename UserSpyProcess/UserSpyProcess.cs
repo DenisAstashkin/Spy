d0a8d165e26376b72f11a07eb20aa88accbb71f0
DenisAstashkin <ast.den27@gmail.com>
2022-07-31 11:25:51 +0400

    Add function ShowWindow for UserSpy

--- UserSpyProcess/UserSpyProcess.cs
+++ UserSpyProcess/UserSpyProcess.cs
@@ -1,7 +1,25 @@
-﻿namespace UserSpyProcess
-{
-    public class UserSpyProcess
-    {
+﻿using System.Diagnostics;
 
+namespace UserSpyProcess
+{
+    public static class UserSpyProcess
+    {
+        public static bool ShowWindow(string path, bool show)
+        {            
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = path,
+                    UseShellExecute = false,
+                    CreateNoWindow = !show
+                });
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
     }
 }
