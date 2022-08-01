627d8d654561269d0efce29ab92090fde62b7703
DenisAstashkin <ast.den27@gmail.com>
2022-08-01 21:21:15 +0400

    Add function check IsEmpty

--- SpyProcess/ProcessModel/ProcessModel.cs
+++ SpyProcess/ProcessModel/ProcessModel.cs
@@ -3,6 +3,24 @@
     public class ProcessModel
     {
         public string ProcessName { get; set; }
-        public DateTime StartProcess { get; set; }
+        public object? StartProcess { get; set; }
+
+
+        public ProcessModel()
+        {
+            ProcessName = string.Empty;
+            StartProcess = null;
+        }
+        public ProcessModel(string ProcessName, object? StartProcess)
+        {
+            this.ProcessName = ProcessName;
+            this.StartProcess = StartProcess;
+        }
+        public bool IsEmpty()
+        {
+            if (ProcessName == string.Empty && StartProcess == null)
+                return true;
+            return false;
+        }
     }
 }
