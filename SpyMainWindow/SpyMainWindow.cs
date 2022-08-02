69ae84e5601d22e73b89f29daee719e026b382b6
DenisAstashkin <ast.den27@gmail.com>
2022-08-03 00:57:08 +0400

    Update Project

--- SpyMainWindow/SpyMainWindow.cs
+++ SpyMainWindow/SpyMainWindow.cs
@@ -1,8 +1,34 @@
 using System;
+using System.Windows;
 
-namespace SpyMainWindow
+namespace spymainwindow
 {
-    public class Class1
+    public static class SpyMainWindow
     {
+        public static bool CloseWindow(Window window)
+        {
+            try
+            {
+                window.Hide();
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+        public static bool ShowWindow(Window window)
+        {
+            try
+            {
+                window.Show();
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
     }
 }
