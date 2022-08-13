2d7a44674f6aa795cf0641854748650b0104f123
DenisAstashkin <ast.den27@gmail.com>
2022-08-14 01:46:16 +0400

    Add Window for kill process

---
+++ UserSpy/RequestWindow/RequestWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using savefile;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace UserSpy
+{ 
+    public partial class RequestWindow : Window
+    {        
+        public string Path { get; private set; }       
+        
+        public RequestWindow(string Title)
+        {            
+            InitializeComponent();              
+            Path = string.Empty;
+            LablInfo.Content = Title;
+        }
+
+        private void SaveButton(object sender, RoutedEventArgs e)
+        {
+            var ArrayStrings = InputInfo.Text.Split(',', StringSplitOptions.RemoveEmptyEntries | 
+                                                         StringSplitOptions.TrimEntries);
+            InputInfo.Clear();
+            foreach (var @string in ArrayStrings)
+            {
+                InputInfo.Text += $"{@string}\n";
+            }
+            MessageBox.Show("Отформатированный текст");            
+            if(MessageBox.Show("Сохранить?", "Вопрос", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                var Dialog = new SaveFileDialog
+                {
+                    Filter = "(*.txt)|*.txt"
+                };
+                Dialog.ShowDialog();
+                Path = Dialog.FileName;
+                if(SaveFile.Save(Dialog.FileName, (StreamWriter sw) =>
+                {
+                    foreach (var @string in ArrayStrings)
+                    {
+                        sw.WriteLine(@string);
+                    }
+                }))
+                {
+                    MessageBox.Show("Сохранено");
+                }
+            }
+        }               
+    }
+}
