38fe20de7694234f704d468a7601e612710b7c94
DenisAstashkin <ast.den27@gmail.com>
2022-08-14 01:45:19 +0400

    Add class SaveFile

---
+++ SaveFile/SaveFile.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace savefile
+{
+    public static class SaveFile
+    {
+        public static bool Save(string path, Action<StreamWriter> HowSaveFile)
+        {
+            try
+            {
+                using (var sw = new StreamWriter(path, false, Encoding.UTF8))
+                {
+                    HowSaveFile(sw);
+                }
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+        public static IEnumerable<string> Read(string path)
+        {
+            var listprocess = new List<string>();
+            try
+            {
+                
+                using (var sr = new StreamReader(path))
+                {
+                    string? process;
+                    while ((process = sr.ReadLine()) != null)
+                    {
+                        listprocess.Add(process);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+            return listprocess as IEnumerable<string>;
+           
+        }
+    }
+}
